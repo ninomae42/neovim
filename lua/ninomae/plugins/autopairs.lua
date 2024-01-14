1c69d78b8d39b260ddc609962de88a5a7b0e3af9
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2024-01-14 19:48:17 +0900

    feat: add autopair plugin

---
+++ lua/ninomae/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+return {
+    "windwp/nvim-autopairs",
+    check_ts=true,
+    event = "InsertEnter",
+    opts = {
+        enable_check_bracket_line = false,
+        map_c_h = true,
+        map_c_w = true,
+    },
+}
